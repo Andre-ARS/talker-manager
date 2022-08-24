ef1344b0be0d8d958be0f205f6fa8f5bc5f5a266
Andre <andre360.ars@gmail.com>
2022-08-24 16:34:23 -0300

    revert

--- services/getTalkers.js
+++ services/getTalkers.js
@@ -1,7 +1,7 @@
 const { readFile } = require('../helpers');
 
 const getTalkers = async (_req, res) => {
-  const path = '../talker.json';
+  const path = './talker.json';
 
   const result = await readFile(path);
   return res.status(200).json(result);
